96c56b2ef62f002c6dc6b7bf2fe61d477cc02cf3
anrobe <anteromatoszabarburu@gmail.com>
2024-10-04 16:27:04 -0500

    fase 3 - Controller_2

--- src/main/java/com/bpb/publications/authors/controller/AuthorController.java
+++ src/main/java/com/bpb/publications/authors/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.bpb.publications.authors.controller;
 import com.bpb.publications.authors.service.AuthorService;
 import com.bpb.publications.authors.vo.AuthorVO;
 import com.bpb.publications.authors.exception.NoRecordsException;
+import com.bpb.publications.authors.exception.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,42 +19,42 @@ public class AuthorController {
     private AuthorService authorService;
 
     @PostMapping("/save")
-    public ResponseEntity<String> saveAuthor(@RequestBody AuthorVO authorVO) {
+    public ResponseEntity<ErrorMessage> saveAuthor(@RequestBody AuthorVO authorVO) {
         boolean isSaved = authorService.save(authorVO);
         if (isSaved) {
-            return new ResponseEntity<>("Author saved successfully", HttpStatus.CREATED);
+            return new ResponseEntity<>(new ErrorMessage("Author saved successfully"), HttpStatus.CREATED);
         } else {
-            return new ResponseEntity<>("Failed to save author", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ErrorMessage("Failed to save author"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteAuthor(@PathVariable Long id) {
+    public ResponseEntity<ErrorMessage> deleteAuthor(@PathVariable Long id) {
         try {
             authorService.deleteById(id);
-            return new ResponseEntity<>("Author deleted successfully", HttpStatus.OK);
+            return new ResponseEntity<>(new ErrorMessage("Author deleted successfully"), HttpStatus.OK);
         } catch (NoRecordsException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(new ErrorMessage(e.getMessage()), HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<AuthorVO>> getAllAuthors() {
+    public ResponseEntity<?> getAllAuthors() {
         try {
             List<AuthorVO> authors = authorService.findAll();
             return new ResponseEntity<>(authors, HttpStatus.OK);
         } catch (NoRecordsException e) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(new ErrorMessage(e.getMessage()), HttpStatus.NO_CONTENT);
         }
     }
 
     @GetMapping("/getId/{id}")
-    public ResponseEntity<AuthorVO> getAuthorById(@PathVariable Long id) {
+    public ResponseEntity<?> getAuthorById(@PathVariable Long id) {
         try {
             AuthorVO authorVO = authorService.findById(id);
             return new ResponseEntity<>(authorVO, HttpStatus.OK);
         } catch (NoRecordsException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(new ErrorMessage(e.getMessage()), HttpStatus.NOT_FOUND);
         }
     }
 }
