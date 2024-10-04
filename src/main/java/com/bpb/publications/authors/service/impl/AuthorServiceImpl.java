09a762a7dd62bc8863e823424512a3ae9248e88b
anrobe <anteromatoszabarburu@gmail.com>
2024-10-04 15:46:51 -0500

    fase 3 - Exception y Service

--- src/main/java/com/bpb/publications/authors/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/bpb/publications/authors/service/impl/AuthorServiceImpl.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Service("authorService")
 public class AuthorServiceImpl implements AuthorService {
 
     @Autowired
