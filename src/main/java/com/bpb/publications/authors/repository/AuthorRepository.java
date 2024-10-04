525275a8dc996cfb2e54b3a051660d52a561ad5d
anrobe <anteromatoszabarburu@gmail.com>
2024-10-04 15:05:44 -0500

    fase 3 - Repository

---
+++ src/main/java/com/bpb/publications/authors/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.bpb.publications.authors.repository;
+
+import com.bpb.publications.authors.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("authorRepository")
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
